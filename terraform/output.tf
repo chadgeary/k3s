7e7b498025ba522e11d1b5f26b045ca6b32390de
chad <chad.geary@gmail.com>
2022-12-19 10:40:08 -0700

    cleanup note

--- terraform/output.tf
+++ terraform/output.tf
@@ -13,6 +13,12 @@ ${data.aws_caller_identity.k3s.account_id}.dkr.ecr.${var.region}.amazonaws.com/$
 # To fetch the kubeconfig from s3
 # and open a tunnel to the k3s cluster
 ./connect.sh
+
+# To cleanup ECR repositories
+repos=$(aws ecr describe-repositories --query "repositories[?starts_with(repositoryName, '${local.prefix}-${local.suffix}')].repositoryName" --output text)
+for repo in $repos; do
+  aws ecr delete-repository --repository-name $repo --force
+done
 EOT
 
 }
